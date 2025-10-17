f88a63bff3dabcbb9987e7f5ba2f49b7ca6d920f
Dave2ooo <david_schweighofer@gmx.at>
2025-10-17 19:06:38 +0200

    fixed merge errors

--- src/lib/globalState.svelte.ts
+++ src/lib/globalState.svelte.ts
@@ -1,5 +1,8 @@
 import type { GlobalState } from "$lib/types/globalState"
+import { GROUP_DEFINITIONS, GROUP_IDS, type GroupId } from "./types/group"
 import { loadFromLocalStorage } from "./utils/localStorage"
+
+export const GLOBAL_STATE_STORAGE_KEY = "globalState"
 
 const createCollapsedCategories = (): Record<GroupId, boolean[]> =>
     Object.fromEntries(
