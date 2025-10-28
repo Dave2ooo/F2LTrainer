d1c871aafc313006b330f06ac6dc788e43ffaa07
Dave2ooo <david_schweighofer@gmx.at>
2025-10-28 09:29:15 +0100

    reset twisty player on next case

--- src/lib/components/TrainView/TrainView.svelte
+++ src/lib/components/TrainView/TrainView.svelte
@@ -6,6 +6,9 @@
 		advanceToPreviousTrainCase,
 		trainState
 	} from '$lib/trainCaseQueue.svelte';
+	import { tick } from 'svelte';
+
+	let twistyPlayerRef: any;
 
 	// local reactive mirror of the global state.current
 	let currentTrainCase = $state(trainState.current);
@@ -15,7 +18,24 @@
 		// console.log('Current Train Case:', trainState.current);
 		currentTrainCase = trainState.current;
 	});
+
+	async function onNext() {
+		advanceToNextTrainCase();
+		// wait for state/DOM to settle so TwistyPlayer receives the new props
+		await tick();
+		twistyPlayerRef?.jumpToStart();
+		twistyPlayerRef?.resetView();
+	}
+
+	function handleKeydown(event: KeyboardEvent) {
+		if (event.code === 'Space') {
+			event.preventDefault();
+			onNext();
+		}
+	}
 </script>
+
+<svelte:window onkeydown={handleKeydown} />
 
 <h2>Group: {currentTrainCase.groupId}, Case: {currentTrainCase.caseId}</h2>
 <span>{currentTrainCase.scramble}</span>
@@ -23,9 +43,10 @@
 <span>{currentTrainCase.alg}</span>
 
 <Button onclick={advanceToPreviousTrainCase}>Previous</Button>
-<Button onclick={advanceToNextTrainCase}>Next</Button>
+<Button onclick={onNext}>Next</Button>
 
 <TwistyPlayer
+	bind:this={twistyPlayerRef}
 	alg={currentTrainCase?.alg}
 	setupAlg={currentTrainCase.scramble}
 	experimentalDragInput="auto"
