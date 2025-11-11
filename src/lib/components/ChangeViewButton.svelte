e153cbed75489e59a8e4ed675f0fbbdb23e7d64b
Dave2ooo <david_schweighofer@gmx.at>
2025-11-11 07:43:16 +0100

    Enhance ChangeViewButton styling for improved responsiveness

--- src/lib/components/ChangeViewButton.svelte
+++ src/lib/components/ChangeViewButton.svelte
@@ -14,7 +14,10 @@
 	}
 </script>
 
-<Button onclick={onToggleView} class="fixed right-4 bottom-4 z-50">
+<Button
+	onclick={onToggleView}
+	class="fixed right-4 bottom-4 z-50 text-base sm:text-lg md:text-xl 2xl:text-2xl"
+>
 	{buttonText}
 </Button>
 
