5a8ae7b3d651fc589728b0cedd7d1121f9af96ad
Dave2ooo <david_schweighofer@gmx.at>
2025-11-14 08:25:46 +0100

    Refactor TooltipButton component for improved readability and maintainability

--- src/lib/components/Modals/TooltipButton.svelte
+++ src/lib/components/Modals/TooltipButton.svelte
@@ -6,7 +6,11 @@
 	export let icon: any;
 </script>
 
-<Button {id} class="bg-transparent p-1 hover:bg-transparent focus:bg-transparent" type="button">
+<Button
+	{id}
+	class="bg-transparent p-1 hover:bg-transparent focus:bg-transparent dark:bg-transparent dark:hover:bg-transparent dark:focus:bg-transparent"
+	type="button"
+>
 	<svelte:component this={icon} class="text-primary-600" />
 </Button>
 <Tooltip trigger="click" triggeredBy={`#${id}`}>{tooltip}</Tooltip>
