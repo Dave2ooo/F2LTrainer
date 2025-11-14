b91bac93214f9dedf90168e70834a3b90f621b90
Dave2ooo <david_schweighofer@gmx.at>
2025-11-14 17:27:13 +0100

    Enhance AccordionItem button styling for improved visibility and consistency

--- src/lib/components/Accordion/AccordionItem.svelte
+++ src/lib/components/Accordion/AccordionItem.svelte
@@ -47,16 +47,15 @@
 	<button
 		type="button"
 		onclick={handleToggle}
-		class={`flex w-full items-center justify-between border-b border-gray-200 px-2 py-2 text-left font-medium text-gray-900 group-first:rounded-t-xl group-first:border-t hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:ring-gray-800 ${
+		class={`flex w-full items-center justify-start border-b border-gray-200 px-2 py-2 text-left font-medium text-theme-text group-first:rounded-t-xl group-first:border-t hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:ring-gray-800 ${
 			open ? 'bg-gray-100 dark:bg-gray-800' : ''
 		} ${headerClass}`}
 		style="min-height:2.7rem; gap:0.9rem; line-height:1.8;"
 		aria-expanded={open}
 	>
 		{#if header}
-			{@render header()}
 			<svg
-				class="h-3 w-3 text-gray-800 transition-transform dark:text-white {open
+				class="mr-0 size-3 text-theme-text transition-transform md:size-4 {open
 					? 'rotate-180'
 					: ''}"
 				aria-hidden="true"
@@ -72,6 +71,7 @@
 					d="m1 1 4 4 4-4"
 				/>
 			</svg>
+			{@render header()}
 		{/if}
 	</button>
 </h2>
