fb57423cbd202d84395377bd1e04795f5e9a1689
Dave2ooo <david_schweighofer@gmx.at>
2025-11-16 15:20:25 +0100

    Added icons for possible later use

--- src/lib/components/ChangeViewButton.svelte
+++ src/lib/components/ChangeViewButton.svelte
@@ -2,6 +2,7 @@
 	import { globalState } from '$lib/globalState.svelte';
 	import { Button } from 'flowbite-svelte';
 	import { getNumberOfSelectedCases } from '$lib/trainCaseQueue.svelte';
+	import { MousePointer2, Play } from '@lucide/svelte';
 
 	let buttonText = $derived(
 		globalState.view == 'select'
@@ -19,6 +20,11 @@
 	class="fixed right-4 bottom-4 z-50 text-base sm:text-lg md:text-xl 2xl:text-2xl"
 >
 	{buttonText}
+	<!-- {#if globalState.view == 'select'}
+		<Play />
+	{:else}
+		<MousePointer2 />
+	{/if} -->
 </Button>
 
 <!-- Spacer to allow scrolling past the fixed button; matches button height -->
