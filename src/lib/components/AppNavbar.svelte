bbf3e08d627f6e598ee2469b3c89f31ca4190775
Dave2ooo <david_schweighofer@gmx.at>
2025-11-25 06:55:14 +0100

    changed responsive breakpoint of Navbar

--- src/lib/components/AppNavbar.svelte
+++ src/lib/components/AppNavbar.svelte
@@ -17,9 +17,9 @@
 	let { settingsRef, feedbackRef, helpRef, onExportURL }: Props = $props();
 </script>
 
-<Navbar fluid={true} color="none" class="bg-gray-100 px-4 py-2 md:py-0 dark:bg-gray-900">
+<Navbar breakpoint="sm" fluid={true} color="none" class="bg-gray-100 px-4 py-2 sm:py-0 dark:bg-gray-900">
 	<NavBrand>
-		<img src={resolve(`/logo.svg`, {})} class="me-3 h-9 sm:h-12" alt="F2L Trainer Logo" />
+		<img src={resolve(`/logo.svg`, {})} class="me-3 h-9 md:h-12" alt="F2L Trainer Logo" />
 		<span class="self-center whitespace-nowrap text-xl md:text-3xl font-semibold dark:text-white"
 			>F2L Trainer</span
 		>
@@ -36,33 +36,33 @@
 		/>
 	</div>
 	<NavUl>
-		<li class="mx-1 my-2 md:my-0">
+		<li class="mx-1 my-2 sm:my-0">
 			<Button
 				class="flex items-center justify-start bg-transparent p-1 hover:bg-transparent dark:bg-transparent dark:hover:bg-transparent"
 				onclick={() => helpRef.openModal()}
 			>
 				<CircleQuestionMark class="size-8 text-primary-600 md:size-9" />
-				<span class="ml-4 text-lg font-medium text-gray-900 dark:text-white md:hidden">Help</span>
+				<span class="ml-4 text-lg font-medium text-gray-900 dark:text-white sm:hidden">Help</span>
 			</Button>
 			<Tooltip placement="bottom">Help</Tooltip>
 		</li>
-		<li class="mx-1 my-2 md:my-0">
+		<li class="mx-1 my-2 sm:my-0">
 			<Button
 				class="flex items-center justify-start bg-transparent p-1 hover:bg-transparent dark:bg-transparent dark:hover:bg-transparent"
 				onclick={() => feedbackRef.openModal()}
 			>
 				<MessageCircle class="size-8 text-primary-600 md:size-9" />
-				<span class="ml-4 text-lg font-medium text-gray-900 dark:text-white md:hidden">Send Feedback</span>
+				<span class="ml-4 text-lg font-medium text-gray-900 dark:text-white sm:hidden">Send Feedback</span>
 			</Button>
 			<Tooltip placement="bottom">Send Feedback</Tooltip>
 		</li>
-		<li class="mx-1 my-2 md:my-0">
+		<li class="mx-1 my-2 sm:my-0">
 			<Button
 				class="flex items-center justify-start bg-transparent p-1 hover:bg-transparent dark:bg-transparent dark:hover:bg-transparent"
 				onclick={onExportURL}
 			>
 				<Share2 class="size-8 text-primary-600 md:size-9" />
-				<span class="ml-4 text-lg font-medium text-gray-900 dark:text-white md:hidden">Export to URL</span>
+				<span class="ml-4 text-lg font-medium text-gray-900 dark:text-white sm:hidden">Export to URL</span>
 			</Button>
 			<Tooltip placement="bottom">Export to URL</Tooltip>
 		</li>
