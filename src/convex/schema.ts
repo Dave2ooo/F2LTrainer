68bb4f0ab61eb25359c157d843968035239c88c6
Dave2ooo <david_schweighofer@gmx.at>
2026-01-27 19:13:44 +0100

    added solve schema

---
+++ src/convex/schema.ts
@@ -0,0 +1,21 @@
+import { defineSchema, defineTable } from 'convex/server';
+import { v } from 'convex/values';
+
+export default defineSchema({
+	solves: defineTable({
+		id: v.string(), // your own ID, separate from Convex _id
+		groupId: v.string(), // or v.id("groups") if this references another table
+		caseId: v.string(),
+		time: v.optional(v.number()),
+
+		timestamp: v.number(), // ms since epoch (or whatever you use)
+		auf: v.string(),
+		side: v.string(),
+		scrambleSelection: v.number(),
+		sessionId: v.optional(v.string()),
+
+		recognitionTime: v.optional(v.number()),
+		executionTime: v.optional(v.number()),
+		trainMode: v.string()
+	})
+});
