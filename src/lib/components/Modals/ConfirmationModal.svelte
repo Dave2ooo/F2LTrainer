c7c04a506f34adb2e00c738c2b3ad1e981bb1c29
Dave2ooo <david_schweighofer@gmx.at>
2026-03-14 16:48:36 +0100

    formatted

--- src/lib/components/Modals/ConfirmationModal.svelte
+++ src/lib/components/Modals/ConfirmationModal.svelte
@@ -34,7 +34,11 @@
 				{message}
 			</div>
 		</h3>
-		<Button color={confirmColor} class="me-2 gap-2" onclick={handleConfirm}><Trash2 class="size-4" />{confirmText}</Button>
-		<Button color="alternative" class="gap-2" onclick={() => (open = false)}>Cancel<X class="size-4" /></Button>
+		<Button color={confirmColor} class="me-2 gap-2" onclick={handleConfirm}
+			><Trash2 class="size-4" />{confirmText}</Button
+		>
+		<Button color="alternative" class="gap-2" onclick={() => (open = false)}
+			>Cancel<X class="size-4" /></Button
+		>
 	</div>
 </Modal>
